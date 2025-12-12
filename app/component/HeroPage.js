d53738889a4ba49e4bfed26a4c57e4a36be90ad7
Nayeem0011 <sknayeemislam384@gmail.com>
2025-12-12 20:58:06 +0600

    better help project

--- app/component/HeroPage.js
+++ app/component/HeroPage.js
@@ -1,7 +1,7 @@
 'use client';
 
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
 import GroupLine from "../svg/GroupLine";
 
 // import Image from 'next/image';
--- app/component/NavBar.js
+++ app/component/NavBar.js
@@ -2,44 +2,73 @@
 
 import Image from 'next/image';
 import nav_logo from './image/nav_logo.svg';
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const NavBar = () => {
-  // const [active, setActive] = useState("trading");
+  const [activeIndex, setActiveIndex] = useState(0); // first item active
+  // Scroll state
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // Scroll Detect Function
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      // scrolling down → navbar hide
+      setShowNav(false);
+    } else {
+      // scrolling up → navbar show
+      setShowNav(true);
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => window.removeEventListener("scroll", controlNavbar);
+  }, [lastScrollY]);
 
   const menuItems = ["Home", "Business", "About", "Advice", "EAP", "FAQ", "Reviews"];
 
   return (
-    <div className="relative inset-0 z-10 pt-6 px-4 md:px-8">
-      <nav className="h-[54px] gap-6 md:gap-[84px] flex flex-col md:flex-row items-center justify-between md:justify-center">
-      
-        {/* Logo */}
-        <div>
-          <Image
-            src={nav_logo}
-            alt="logo"
-            width={214}
-            height={30}
-          />
-        </div>
+    <div className="relative inset-0 z-10 pt-7 px-28">
+      <div className={`top-0 left-0 transition-all duration-800
+        ${showNav ? "translate-y-0" : "-translate-y-[150px]"}`}>
+        <nav className="h-[62px] gap-6 md:gap-[100px] flex items-center justify-between">
 
-        {/* Menu Items */}
-        <div>
-          <ul className="text-[#878584] w-full md:w-[697px] h-11 flex items-center justify-between rounded-[100px] px-6 md:px-10 py-1.5 gap-x-4 md:gap-x-0 transition-all duration-300">
-            {menuItems.map((item, index) => (
-              <li
-                key={index}
-                className="font-medium text-sm md:text-[16px] leading-[140%] tracking-[0%] cursor-pointer"
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
+          {/* Logo */}
+          <Image src={nav_logo} alt="logo" className='w-[214px] h-[30px]' />
 
-        {/* Auth Buttons */}
+          {/* Desktop Menu */}
+          <div>
 
-      </nav>
+            <ul className="w-full md:w-[697px] h-11 flex items-center justify-between transition-all duration-300">
+              {menuItems.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => setActiveIndex(index)}
+                  className={`font-normal py-2.5 px-5 text-[20px] leading-[120%] tracking-[0] cursor-pointer rounded-full  transition-all duration-300
+        ${activeIndex === index
+                      ? "bg-[#15081459] text-white opacity-100"
+                      : "text-white opacity-60"
+                    }
+        hover:bg-[#15081459] hover:text-white hover:opacity-100
+      `}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+
+          </div>
+
+          {/* Auth Buttons */}
+          <div className='border border-white gap-2 rounded-[182px] px-10 py-4 cursor-pointer'>
+            <button className='text-white font-normal text-[18px] leading-[120%] tracking-[0%] cursor-pointer'>
+              Log in
+            </button>
+          </div>
+
+        </nav>
+      </div>
     </div>
   );
 };
